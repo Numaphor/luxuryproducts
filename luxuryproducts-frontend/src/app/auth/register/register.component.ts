a3ebf37634ab87be464833ea242f72bc17e4ad25
Jeffrey Boone <jefdayt@gmail.com>
2024-05-20 15:41:46 +0200

    chore: Update application.properties and register.component.ts

--- luxuryproducts-frontend/src/app/auth/register/register.component.ts
+++ luxuryproducts-frontend/src/app/auth/register/register.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AuthService} from '../auth.service';
-import {AuthResponse} from "../auth-response.model";
-import {Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { AuthResponse } from "../auth-response.model";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-register',
