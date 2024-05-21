e20a643bfd36784f5393dd093f4c9252d1741012
Jeffrey Boone <jefdayt@gmail.com>
2024-05-21 14:28:46 +0200

    feat: Add Giftcard model, service, and component
    This commit adds the Giftcard model, service, and component to the luxuryproducts-frontend application. The Giftcard model defines the structure of a gift card with properties like id, balance, code, pin, and owner. The Giftcard service provides methods for retrieving gift cards from the backend API. The Giftcard component is responsible for displaying gift card information and handling user interactions related to gift cards.

--- luxuryproducts-frontend/src/app/giftcard/giftcard.component.ts
+++ luxuryproducts-frontend/src/app/giftcard/giftcard.component.ts
@@ -1,14 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Giftcard } from '../models/giftcard.model';
+import { GiftcardService } from '../services/giftcard.service';
 
 @Component({
-    selector: 'app-giftcard',
-    standalone: true,
-    imports: [
-        CommonModule,
-    ],
-    templateUrl: './giftcard.component.html',
-    styleUrl: './giftcard.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-giftcard',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  templateUrl: './giftcard.component.html',
+  styleUrl: './giftcard.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GiftcardComponent { }
+export class GiftcardComponent implements OnInit {
+  giftcards: Giftcard[];
+
+  constructor(private giftcardService: GiftcardService) { }
+
+  ngOnInit(): void {
+    this.giftcardService.getGiftcards().subscribe(giftcards => {
+      this.giftcards = giftcards;
+    });
+
+
+
+  }
+
+}
